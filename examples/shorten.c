666701eb5688767a544902d08e99cf17984eb180
gomjellie <gomjellie@gmail.com>
2020-11-20 21:46:33 +0900

    change demo gif

--- examples/shorten.c
+++ examples/shorten.c
@@ -32,6 +32,6 @@ int main() {
 
     printf(BLINK("ㄷㄷㄷㅈ") "\n");
     printf(BLINK(MAGENTA("DUDUDUNGA")) "\n");
-    printf(ITALIC("기울인" BOLD("굵은 글씨")));
+    printf(ITALIC("ITALIC " BOLD("BOLD TEXT")) "\n");
     return 0;
 }
